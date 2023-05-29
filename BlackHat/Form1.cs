949adfb2d0e246e95bd4aeee9b438be51062985b
Milano <milanoward22@gmail.com>
2023-05-29 02:30:14 +0100

    main

--- BlackHat/Form1.cs
+++ BlackHat/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -50,7 +51,7 @@ namespace BlackHat
                         else
                         if (FileCheck.Checked)//deletes files
                         {
-                            sw.Write("\n@echo off\r\n\r\n:: BatchGotAdmin\r\n:-------------------------------------\r\nREM  --> Check for permissions\r\n    IF \"%PROCESSOR_ARCHITECTURE%\" EQU \"amd64\" (\r\n>nul 2>&1 \"%SYSTEMROOT%\\SysWOW64\\cacls.exe\" \"%SYSTEMROOT%\\SysWOW64\\config\\system\"\r\n) ELSE (\r\n>nul 2>&1 \"%SYSTEMROOT%\\system32\\cacls.exe\" \"%SYSTEMROOT%\\system32\\config\\system\"\r\n)\r\n\r\nREM --> If error flag set, we do not have admin.\r\nif '%errorlevel%' NEQ '0' (\r\n    echo Requesting administrative privileges...\r\n    goto UACPrompt\r\n) else ( goto gotAdmin )\r\n\r\n:UACPrompt\r\n    echo Set UAC = CreateObject^(\"Shell.Application\"^) > \"%temp%\\getadmin.vbs\"\r\n    set params= %*\r\n    echo UAC.ShellExecute \"cmd.exe\", \"/c \"\"%~s0\"\" %params:\"=\"\"%\", \"\", \"runas\", 1 >> \"%temp%\\getadmin.vbs\"\r\n\r\n    \"%temp%\\getadmin.vbs\"\r\n    del \"%temp%\\getadmin.vbs\"\r\n    exit /B\r\n\r\n:gotAdmin\r\n    pushd \"%CD%\"\r\n    CD /D \"%~dp0\"\r\n:--------------------------------------    \r\n\\ndel \"" + deltext.Text+"\n");
+                            sw.Write("del " + deltext.Text);
                         }
                         else
                         if (AppCheck.Checked)//starts app
@@ -90,7 +91,7 @@ namespace BlackHat
 
                         if (FileCheck.Checked)
                         {
-                            sw.Write("\n@echo off\r\n\r\n:: BatchGotAdmin\r\n:-------------------------------------\r\nREM  --> Check for permissions\r\n    IF \"%PROCESSOR_ARCHITECTURE%\" EQU \"amd64\" (\r\n>nul 2>&1 \"%SYSTEMROOT%\\SysWOW64\\cacls.exe\" \"%SYSTEMROOT%\\SysWOW64\\config\\system\"\r\n) ELSE (\r\n>nul 2>&1 \"%SYSTEMROOT%\\system32\\cacls.exe\" \"%SYSTEMROOT%\\system32\\config\\system\"\r\n)\r\n\r\nREM --> If error flag set, we do not have admin.\r\nif '%errorlevel%' NEQ '0' (\r\n    echo Requesting administrative privileges...\r\n    goto UACPrompt\r\n) else ( goto gotAdmin )\r\n\r\n:UACPrompt\r\n    echo Set UAC = CreateObject^(\"Shell.Application\"^) > \"%temp%\\getadmin.vbs\"\r\n    set params= %*\r\n    echo UAC.ShellExecute \"cmd.exe\", \"/c \"\"%~s0\"\" %params:\"=\"\"%\", \"\", \"runas\", 1 >> \"%temp%\\getadmin.vbs\"\r\n\r\n    \"%temp%\\getadmin.vbs\"\r\n    del \"%temp%\\getadmin.vbs\"\r\n    exit /B\r\n\r\n:gotAdmin\r\n    pushd \"%CD%\"\r\n    CD /D \"%~dp0\"\r\n:--------------------------------------    \r\n\\ndel \"" + deltext.Text + "\n");
+                            sw.Write("del " + deltext.Text);
                         }
                         else
                         if (AppCheck.Checked)
@@ -135,7 +136,7 @@ namespace BlackHat
 
                         if (FileCheck.Checked)
                         {
-                            sw.Write("\n@echo off\r\n\r\n:: BatchGotAdmin\r\n:-------------------------------------\r\nREM  --> Check for permissions\r\n    IF \"%PROCESSOR_ARCHITECTURE%\" EQU \"amd64\" (\r\n>nul 2>&1 \"%SYSTEMROOT%\\SysWOW64\\cacls.exe\" \"%SYSTEMROOT%\\SysWOW64\\config\\system\"\r\n) ELSE (\r\n>nul 2>&1 \"%SYSTEMROOT%\\system32\\cacls.exe\" \"%SYSTEMROOT%\\system32\\config\\system\"\r\n)\r\n\r\nREM --> If error flag set, we do not have admin.\r\nif '%errorlevel%' NEQ '0' (\r\n    echo Requesting administrative privileges...\r\n    goto UACPrompt\r\n) else ( goto gotAdmin )\r\n\r\n:UACPrompt\r\n    echo Set UAC = CreateObject^(\"Shell.Application\"^) > \"%temp%\\getadmin.vbs\"\r\n    set params= %*\r\n    echo UAC.ShellExecute \"cmd.exe\", \"/c \"\"%~s0\"\" %params:\"=\"\"%\", \"\", \"runas\", 1 >> \"%temp%\\getadmin.vbs\"\r\n\r\n    \"%temp%\\getadmin.vbs\"\r\n    del \"%temp%\\getadmin.vbs\"\r\n    exit /B\r\n\r\n:gotAdmin\r\n    pushd \"%CD%\"\r\n    CD /D \"%~dp0\"\r\n:--------------------------------------    \r\n\\ndel \"" + deltext.Text + "\n");
+                            sw.Write("del " + deltext.Text);
                         }
                         else
                         if (ShutdownBox.Checked)
@@ -176,7 +177,7 @@ namespace BlackHat
                     using (Stream s = File.Open(saveFileDialog1.FileName, FileMode.Create))
                     using (StreamWriter sw = new StreamWriter(s))
                     {
-                        sw.Write("\n@echo off\r\n\r\n:: BatchGotAdmin\r\n:-------------------------------------\r\nREM  --> Check for permissions\r\n    IF \"%PROCESSOR_ARCHITECTURE%\" EQU \"amd64\" (\r\n>nul 2>&1 \"%SYSTEMROOT%\\SysWOW64\\cacls.exe\" \"%SYSTEMROOT%\\SysWOW64\\config\\system\"\r\n) ELSE (\r\n>nul 2>&1 \"%SYSTEMROOT%\\system32\\cacls.exe\" \"%SYSTEMROOT%\\system32\\config\\system\"\r\n)\r\n\r\nREM --> If error flag set, we do not have admin.\r\nif '%errorlevel%' NEQ '0' (\r\n    echo Requesting administrative privileges...\r\n    goto UACPrompt\r\n) else ( goto gotAdmin )\r\n\r\n:UACPrompt\r\n    echo Set UAC = CreateObject^(\"Shell.Application\"^) > \"%temp%\\getadmin.vbs\"\r\n    set params= %*\r\n    echo UAC.ShellExecute \"cmd.exe\", \"/c \"\"%~s0\"\" %params:\"=\"\"%\", \"\", \"runas\", 1 >> \"%temp%\\getadmin.vbs\"\r\n\r\n    \"%temp%\\getadmin.vbs\"\r\n    del \"%temp%\\getadmin.vbs\"\r\n    exit /B\r\n\r\n:gotAdmin\r\n    pushd \"%CD%\"\r\n    CD /D \"%~dp0\"\r\n:--------------------------------------    \r\n\\ndel \"" + deltext.Text + "\n");
+                        sw.Write("del " + deltext.Text);
 
                         if (ShutdownBox.Checked)
                         {
@@ -241,7 +242,7 @@ namespace BlackHat
                         else
                         if(FileCheck.Checked)
                         {
-                            sw.Write("\n@echo off\r\n\r\n:: BatchGotAdmin\r\n:-------------------------------------\r\nREM  --> Check for permissions\r\n    IF \"%PROCESSOR_ARCHITECTURE%\" EQU \"amd64\" (\r\n>nul 2>&1 \"%SYSTEMROOT%\\SysWOW64\\cacls.exe\" \"%SYSTEMROOT%\\SysWOW64\\config\\system\"\r\n) ELSE (\r\n>nul 2>&1 \"%SYSTEMROOT%\\system32\\cacls.exe\" \"%SYSTEMROOT%\\system32\\config\\system\"\r\n)\r\n\r\nREM --> If error flag set, we do not have admin.\r\nif '%errorlevel%' NEQ '0' (\r\n    echo Requesting administrative privileges...\r\n    goto UACPrompt\r\n) else ( goto gotAdmin )\r\n\r\n:UACPrompt\r\n    echo Set UAC = CreateObject^(\"Shell.Application\"^) > \"%temp%\\getadmin.vbs\"\r\n    set params= %*\r\n    echo UAC.ShellExecute \"cmd.exe\", \"/c \"\"%~s0\"\" %params:\"=\"\"%\", \"\", \"runas\", 1 >> \"%temp%\\getadmin.vbs\"\r\n\r\n    \"%temp%\\getadmin.vbs\"\r\n    del \"%temp%\\getadmin.vbs\"\r\n    exit /B\r\n\r\n:gotAdmin\r\n    pushd \"%CD%\"\r\n    CD /D \"%~dp0\"\r\n:--------------------------------------    \r\n\\ndel \"" + deltext.Text + "\n");
+                            sw.Write("del " + deltext.Text);
                         }
                         else
                         if (CrashCheck.Checked)
@@ -288,7 +289,7 @@ namespace BlackHat
                         else
                         if (FileCheck.Checked)
                         {
-                            sw.Write("\n@echo off\r\n\r\n:: BatchGotAdmin\r\n:-------------------------------------\r\nREM  --> Check for permissions\r\n    IF \"%PROCESSOR_ARCHITECTURE%\" EQU \"amd64\" (\r\n>nul 2>&1 \"%SYSTEMROOT%\\SysWOW64\\cacls.exe\" \"%SYSTEMROOT%\\SysWOW64\\config\\system\"\r\n) ELSE (\r\n>nul 2>&1 \"%SYSTEMROOT%\\system32\\cacls.exe\" \"%SYSTEMROOT%\\system32\\config\\system\"\r\n)\r\n\r\nREM --> If error flag set, we do not have admin.\r\nif '%errorlevel%' NEQ '0' (\r\n    echo Requesting administrative privileges...\r\n    goto UACPrompt\r\n) else ( goto gotAdmin )\r\n\r\n:UACPrompt\r\n    echo Set UAC = CreateObject^(\"Shell.Application\"^) > \"%temp%\\getadmin.vbs\"\r\n    set params= %*\r\n    echo UAC.ShellExecute \"cmd.exe\", \"/c \"\"%~s0\"\" %params:\"=\"\"%\", \"\", \"runas\", 1 >> \"%temp%\\getadmin.vbs\"\r\n\r\n    \"%temp%\\getadmin.vbs\"\r\n    del \"%temp%\\getadmin.vbs\"\r\n    exit /B\r\n\r\n:gotAdmin\r\n    pushd \"%CD%\"\r\n    CD /D \"%~dp0\"\r\n:--------------------------------------    \r\n\\ndel \"" + deltext.Text + "\n");
+                            sw.Write("del " + deltext.Text);
                         }
                         else
                         if (Taskkillcheck.Checked)
@@ -336,7 +337,7 @@ namespace BlackHat
                         else
                         if (FileCheck.Checked)
                         {
-                            sw.Write("\n@echo off\r\n\r\n:: BatchGotAdmin\r\n:-------------------------------------\r\nREM  --> Check for permissions\r\n    IF \"%PROCESSOR_ARCHITECTURE%\" EQU \"amd64\" (\r\n>nul 2>&1 \"%SYSTEMROOT%\\SysWOW64\\cacls.exe\" \"%SYSTEMROOT%\\SysWOW64\\config\\system\"\r\n) ELSE (\r\n>nul 2>&1 \"%SYSTEMROOT%\\system32\\cacls.exe\" \"%SYSTEMROOT%\\system32\\config\\system\"\r\n)\r\n\r\nREM --> If error flag set, we do not have admin.\r\nif '%errorlevel%' NEQ '0' (\r\n    echo Requesting administrative privileges...\r\n    goto UACPrompt\r\n) else ( goto gotAdmin )\r\n\r\n:UACPrompt\r\n    echo Set UAC = CreateObject^(\"Shell.Application\"^) > \"%temp%\\getadmin.vbs\"\r\n    set params= %*\r\n    echo UAC.ShellExecute \"cmd.exe\", \"/c \"\"%~s0\"\" %params:\"=\"\"%\", \"\", \"runas\", 1 >> \"%temp%\\getadmin.vbs\"\r\n\r\n    \"%temp%\\getadmin.vbs\"\r\n    del \"%temp%\\getadmin.vbs\"\r\n    exit /B\r\n\r\n:gotAdmin\r\n    pushd \"%CD%\"\r\n    CD /D \"%~dp0\"\r\n:--------------------------------------    \r\n\\ndel \"" + deltext.Text + "\n");
+                            sw.Write("del " + deltext.Text);
                         }
                         else
                         if (Taskkillcheck.Checked)
@@ -384,7 +385,7 @@ namespace BlackHat
                         else
                         if (FileCheck.Checked)
                         {
-                            sw.Write("\n@echo off\r\n\r\n:: BatchGotAdmin\r\n:-------------------------------------\r\nREM  --> Check for permissions\r\n    IF \"%PROCESSOR_ARCHITECTURE%\" EQU \"amd64\" (\r\n>nul 2>&1 \"%SYSTEMROOT%\\SysWOW64\\cacls.exe\" \"%SYSTEMROOT%\\SysWOW64\\config\\system\"\r\n) ELSE (\r\n>nul 2>&1 \"%SYSTEMROOT%\\system32\\cacls.exe\" \"%SYSTEMROOT%\\system32\\config\\system\"\r\n)\r\n\r\nREM --> If error flag set, we do not have admin.\r\nif '%errorlevel%' NEQ '0' (\r\n    echo Requesting administrative privileges...\r\n    goto UACPrompt\r\n) else ( goto gotAdmin )\r\n\r\n:UACPrompt\r\n    echo Set UAC = CreateObject^(\"Shell.Application\"^) > \"%temp%\\getadmin.vbs\"\r\n    set params= %*\r\n    echo UAC.ShellExecute \"cmd.exe\", \"/c \"\"%~s0\"\" %params:\"=\"\"%\", \"\", \"runas\", 1 >> \"%temp%\\getadmin.vbs\"\r\n\r\n    \"%temp%\\getadmin.vbs\"\r\n    del \"%temp%\\getadmin.vbs\"\r\n    exit /B\r\n\r\n:gotAdmin\r\n    pushd \"%CD%\"\r\n    CD /D \"%~dp0\"\r\n:--------------------------------------    \r\n\\ndel \"" + deltext.Text + "\n");
+                            sw.Write("del " + deltext.Text);
                         }
                         else
                         if (Taskkillcheck.Checked)
@@ -425,6 +426,11 @@ namespace BlackHat
                 MessageBox.Show("Please update to a newer version.", "Update", MessageBoxButtons.OK,MessageBoxIcon.Information);
                 System.Diagnostics.Process.Start("https://github.com/MavenCoding157/BlackHat");
             }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
